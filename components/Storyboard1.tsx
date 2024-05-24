d1eb498b7e83f3660301f75dd185b9774d6bd5b9
dazeign <frankpeterani@gmail.com>
2024-05-24 15:31:09 +0000

    moving on

--- components/Storyboard1.tsx
+++ components/Storyboard1.tsx
@@ -267,75 +267,77 @@ export default function Storyboard1() {
         </div>
         <div className="origin-top-left -rotate-90 w-[235.31px] h-[305.96px] left-[941.68px] top-[1938.63px] absolute"></div>
       </div>
-      <div className="w-[155px] h-[36.25px] left-[76px] top-[19.88px] absolute">
-        <Image
-          alt={"Pasted Image"}
-          fetchpriority={undefined}
-          loading={"lazy"}
-          width={310}
-          height={74}
-          decoding={"async"}
-          data-nimg={"1"}
-          className={"w-full h-full"}
-          style={{
-            color: "transparent",
-          }}
-          sizes={undefined}
-          srcSet={undefined}
-          src={"/images/tempo-image-20240517T184146655Z.png"}
-          onLoad={() => console.log("onLoad")}
-          onError={() => console.log("onError")}
-        />
-      </div>
-      <div className="w-[254px] left-[1110px] top-[19px] absolute justify-between items-start inline-flex">
-        <div className="px-3.5 py-2 rounded-[100px] border border-yellow-950 justify-start items-center gap-[7px] flex">
-          <div className="w-6 h-6 relative">
-            <Image
-              alt={"Pasted Image"}
-              fetchpriority={undefined}
-              loading={"lazy"}
-              width={96}
-              height={96}
-              decoding={"async"}
-              data-nimg={"1"}
-              className={"w-full h-full"}
-              style={{
-                color: "transparent",
-              }}
-              sizes={undefined}
-              srcSet={undefined}
-              src={"/images/tempo-image-20240517T185344261Z.png"}
-              onLoad={() => console.log("onLoad")}
-              onError={() => console.log("onError")}
-            />
-          </div>
-          <div className="text-yellow-950 text-[22px] font-normal leading-normal tracking-wide">
-            Cart
-          </div>
+      <div className="flex items-center">
+        <div className="left-[76px] top-[19.88px] absolute w-44 h-11">
+          <Image
+            alt={"Pasted Image"}
+            fetchpriority={undefined}
+            loading={"lazy"}
+            width={310}
+            height={74}
+            decoding={"async"}
+            data-nimg={"1"}
+            className={"w-full h-full"}
+            style={{
+              color: "transparent",
+            }}
+            sizes={undefined}
+            srcSet={undefined}
+            src={"/images/tempo-image-20240517T184146655Z.png"}
+            onLoad={() => console.log("onLoad")}
+            onError={() => console.log("onError")}
+          />
         </div>
-        <div className="px-3.5 py-2 rounded-[100px] border border-yellow-950 justify-start items-center gap-[7px] flex">
-          <div className="w-6 h-6 relative">
-            <Image
-              alt={"Pasted Image"}
-              fetchpriority={undefined}
-              loading={"lazy"}
-              width={96}
-              height={96}
-              decoding={"async"}
-              data-nimg={"1"}
-              className={"w-full h-full"}
-              style={{
-                color: "transparent",
-              }}
-              sizes={undefined}
-              srcSet={undefined}
-              src={"/images/tempo-image-20240517T184327200Z.png"}
-              onLoad={() => console.log("onLoad")}
-              onError={() => console.log("onError")}
-            />
+        <div className="w-[254px] left-[1110px] top-[19px] absolute justify-between items-start inline-flex">
+          <div className="px-3.5 py-2 rounded-[100px] border border-yellow-950 justify-start items-center gap-[7px] flex">
+            <div className="w-6 h-6 relative">
+              <Image
+                alt={"Pasted Image"}
+                fetchpriority={undefined}
+                loading={"lazy"}
+                width={96}
+                height={96}
+                decoding={"async"}
+                data-nimg={"1"}
+                className={"w-full h-full"}
+                style={{
+                  color: "transparent",
+                }}
+                sizes={undefined}
+                srcSet={undefined}
+                src={"/images/tempo-image-20240517T185344261Z.png"}
+                onLoad={() => console.log("onLoad")}
+                onError={() => console.log("onError")}
+              />
+            </div>
+            <div className="text-yellow-950 text-[22px] font-normal leading-normal tracking-wide">
+              Cart
+            </div>
           </div>
-          <div className="text-yellow-950 text-[22px] font-normal font-['Agrandir'] leading-normal tracking-wide">
-            Menu
+          <div className="px-3.5 py-2 rounded-[100px] border border-yellow-950 justify-start items-center gap-[7px] flex">
+            <div className="w-6 h-6 relative">
+              <Image
+                alt={"Pasted Image"}
+                fetchpriority={undefined}
+                loading={"lazy"}
+                width={96}
+                height={96}
+                decoding={"async"}
+                data-nimg={"1"}
+                className={"w-full h-full"}
+                style={{
+                  color: "transparent",
+                }}
+                sizes={undefined}
+                srcSet={undefined}
+                src={"/images/tempo-image-20240517T184327200Z.png"}
+                onLoad={() => console.log("onLoad")}
+                onError={() => console.log("onError")}
+              />
+            </div>
+            <div className="text-yellow-950 text-[22px] font-normal font-['Agrandir'] leading-normal tracking-wide">
+              Menu
+            </div>
           </div>
         </div>
       </div>
